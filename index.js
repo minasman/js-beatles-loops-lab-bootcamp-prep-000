f0c4c27c0083d0f30afd9735275e65963cc7e52f
elreymcd@yahoo.com <elreymcd@yahoo.com>
2018-05-06 19:46:59 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,14 @@
+function theBeatlesPlay(musicians, instruments){
+  var arr = [];
+  for(var i=0; i<musicians.length; i++){
+    var newString = `${musicians[i]} plays ${instruments}`
+    arr.push(newString);
+  }
+  return arr;
+}
+
+function johnLennonFacts(facts){
+  while (var i < facts.length) do{
+    
+  }
+}
